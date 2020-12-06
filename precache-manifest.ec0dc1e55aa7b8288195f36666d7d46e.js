bf2365c0f41efe4cc43da7e833095f11247a0283
Patrick Dodd <kasperkain@gmail.com>
2020-12-06 17:00:21 -0500

    Updates

---
+++ precache-manifest.ec0dc1e55aa7b8288195f36666d7d46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47219dd9dc2137209774310568e428c1",
+    "url": "/laptop-customizer/index.html"
+  },
+  {
+    "revision": "f9cbc183b0a32139942e",
+    "url": "/laptop-customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop-customizer/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "f9cbc183b0a32139942e",
+    "url": "/laptop-customizer/static/js/main.3f81a1c4.chunk.js"
+  },
+  {
+    "revision": "499f4b4adf8f43d881a8",
+    "url": "/laptop-customizer/static/js/runtime~main.9cc997fb.js"
+  }
+]);
